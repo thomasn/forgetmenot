d32ba6f6d716fae3fca5ec0ece27d7bffd553e77
Renat Akhmerov <renat@openforgetmenot.com>
2007-05-22 08:22:50 +0000

    Renat's account testing
    git-svn-id: https://forgetmenot-svn.cvsdude.com/main/trunk@57 a23ba2f0-501e-4c28-b130-3ad736a215ff

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -7,4 +7,5 @@ class ApplicationController < ActionController::Base
   
   # Pick a unique cookie name to distinguish our session data from others'
   session :session_key => '_forgetmenot_session_id'
+
 end
