b8dd8c4d60466b142e22b5dd9c31cc62f5dcf717
Yury Kotlyarov <yura@openforgetmenot.com>
2007-05-31 21:53:09 +0000

    adding search by UDAs
    git-svn-id: https://forgetmenot-svn.cvsdude.com/main/trunk@75 a23ba2f0-501e-4c28-b130-3ad736a215ff

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,5 @@
 require 'fileutils'
+
 class Contact < ActiveRecord::Base
   has_and_belongs_to_many :activities
   has_and_belongs_to_many :groups
@@ -6,22 +7,33 @@ class Contact < ActiveRecord::Base
   belongs_to :address2, :class_name => 'Address', :foreign_key => 'address2_id'
   belongs_to :lead_source
 
-  def self.do_acts_as_ferret(dyn_attrs = DynamicAttribute.find(:all))
+  private
+
+  def self.do_acts_as_ferret(attrs = DynamicAttribute.find(:all))
+    acts_as_ferret :additional_fields => attrs.collect { |a| a.name.to_sym }
+    # FIXME: workaround here - drop following line
+    attrs.each { |a| aaf_index.ferret_index.options[:default_field] << a.name }
     drop_index_dir
-    attrs = Contact.content_columns.collect { |c| c.name.to_sym }
-    acts_as_ferret :fields => (attrs + dyn_attrs.collect { |a| a.name.to_sym })
   end
-  
+
   def self.drop_index_dir
-    if File.exists?('index')
+    if File.exists?(aaf_index.ferret_index.options[:path])
       begin
-        Contact.aaf_index.close 
+        aaf_index.close
       rescue
       end
-      FileUtils.rm_rf('index')
+      FileUtils.rm_rf(aaf_index.ferret_index.options[:path])
     end
   end
-  
+
+  def self.create_attributes
+    attrs = DynamicAttribute.find(:all)
+    attrs.each { |a| create_attribute(a, false) }
+    do_acts_as_ferret attrs
+  end
+
+  public
+
   def self.create_attribute(a, recreate_index = true)
     define_method a.name do
       value = DynamicAttributeValue.find_by_dynamic_attribute_id_and_contact_id(a.id, self.id) 
@@ -33,23 +45,15 @@ class Contact < ActiveRecord::Base
       value = DynamicAttributeValue.find_by_dynamic_attribute_id_and_contact_id(a.id, self.id) 
       value = DynamicAttributeValue.new(:contact_id => self.id, :dynamic_attribute_id => a.id) if value.nil?
       value.update_attribute("#{a.type_name}_value", new_value)
+      ferret_update
     end
-    
-    if recreate_index
-      do_acts_as_ferret
-      Contact.load
-    end
+
+    do_acts_as_ferret if recreate_index
   end
-  
-  def self.create_attributes
-    attrs = DynamicAttribute.find(:all)
-    attrs.each { |a| create_attribute(a, false) }
-    do_acts_as_ferret attrs
-  end
-  
+
   create_attributes
   acts_as_taggable
-  
+
   def display_name
     !self.first_name.nil? || !self.last_name.nil? ? "#{self.first_name} #{self.last_name}".strip : "contact ##{self.id}"
   end
