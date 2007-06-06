41a5ee047c1cc1c47c5af86e6898366c09f03da6
Yury Kotlyarov <yura@openforgetmenot.com>
2007-06-06 09:26:49 +0000

    simplifying dyn attrs
    git-svn-id: https://forgetmenot-svn.cvsdude.com/main/trunk@81 a23ba2f0-501e-4c28-b130-3ad736a215ff

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -34,7 +34,7 @@ class Contact < ActiveRecord::Base
     attrs.each { |a| create_attribute(a, false) }
     do_acts_as_ferret attrs
   end
-  
+
   def self.create_attribute(a, recreate_index = true)
     define_method a.name do
       value = DynamicAttributeValue.find_by_dynamic_attribute_id_and_contact_id(a.id, self.id) 
@@ -43,7 +43,7 @@ class Contact < ActiveRecord::Base
     end
 
     define_method "#{a.name}=" do |new_value|
-      value = DynamicAttributeValue.find_or_create_by_dynamic_attribute_id_and_contact_id(a.id, self.id) 
+      value = DynamicAttributeValue.find_or_create_by_dynamic_attribute_id_and_contact_id(a.id, self.id)
       value.update_attribute("#{a.type_name}_value", new_value)
       ferret_update
     end
@@ -57,27 +57,13 @@ class Contact < ActiveRecord::Base
   def display_name
     !self.first_name.nil? || !self.last_name.nil? ? "#{self.first_name} #{self.last_name}".strip : "contact ##{self.id}"
   end
-  
+
   alias_method :old_column_for_attribute, :column_for_attribute
   def column_for_attribute(method_name)
     obj = old_column_for_attribute(method_name)
-    obj.nil? ? DynamicAttribute.find_by_name(method_name) : obj
+    obj.nil? ? DynamicAttribute.find_by_name(method_name).column : obj
   end
-  
-  alias_method :old_update_attributes, :update_attributes
-  def update_attributes(attrs)
-    dynamic_time_attrs = {}
-    attrs.each_pair { |k, v|
-      if k =~ /(\w+)\W(\d)i/
-        dynamic_time_attrs[$1.to_sym] = [] if dynamic_time_attrs[$1.to_sym].nil?
-        dynamic_time_attrs[$1.to_sym][$2.to_i - 1] = v
-      else
-        update_attribute(k, v) if !v.nil? && !v.to_s.strip.empty?
-      end
-    }
-    dynamic_time_attrs.each_pair { |k, v|
-      update_attribute(k, Time.mktime(v[0], v[1], v[2], v[3], v[4]))
-    }
-  end
-  
+
+
 end
+
--- app/models/dynamic_attribute.rb
+++ app/models/dynamic_attribute.rb
@@ -1,7 +1,13 @@
 class DynamicAttribute < ActiveRecord::Base
+
   after_create { |a| Contact.create_attribute(a) }
+
   def type
     self.type_name.to_sym
   end
+
+  def column
+    ActiveRecord::ConnectionAdapters::Column.new(self.name, nil, self.type_name)
+  end
 end
-  
+
